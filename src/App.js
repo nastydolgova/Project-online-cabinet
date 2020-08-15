09a167e5988c2dfc6d47efce96abc7d5fd032e6c
Laimis <62464661+Laimik@users.noreply.github.com>
2020-08-15 15:34:29 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Dashboard from '../src/Components/Dashboard'
+
+
+
+function App() {
+  return (
+    <div>
+    
+
+    <Dashboard />
+
+    </div>
+  );
+}
+
+export default App;
